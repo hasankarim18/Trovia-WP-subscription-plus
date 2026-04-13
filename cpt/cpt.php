93199fdd8510717728a41d2171c102776e222e64
hasankarim18 <hallysust@gmail.com>
2026-04-13 16:06:31 +0600

    making show_in_menu false for cpt

--- cpt/cpt.php
+++ cpt/cpt.php
@@ -1,17 +1,18 @@
 <?php
-function cptui_register_my_cpts_twsp_subscriber() {
+function cptui_register_my_cpts_twsp_subscriber()
+{
 
 	/**
 	 * Post Type: Subscribers.
 	 */
 
 	$labels = [
-		"name" => esc_html__( "Subscribers", "twentytwentyfive" ),
-		"singular_name" => esc_html__( "Subscriber", "twentytwentyfive" ),
+		"name" => esc_html__("Subscribers", "twentytwentyfive"),
+		"singular_name" => esc_html__("Subscriber", "twentytwentyfive"),
 	];
 
 	$args = [
-		"label" => esc_html__( "Subscribers", "twentytwentyfive" ),
+		"label" => esc_html__("Subscribers", "twentytwentyfive"),
 		"labels" => $labels,
 		"description" => "",
 		"public" => false,
@@ -22,7 +23,7 @@ function cptui_register_my_cpts_twsp_subscriber() {
 		"rest_controller_class" => "WP_REST_Posts_Controller",
 		"rest_namespace" => "wp/v2",
 		"has_archive" => false,
-		"show_in_menu" => true,
+		"show_in_menu" => false,
 		"show_in_nav_menus" => true,
 		"delete_with_user" => false,
 		"exclude_from_search" => true,
@@ -30,30 +31,31 @@ function cptui_register_my_cpts_twsp_subscriber() {
 		"map_meta_cap" => true,
 		"hierarchical" => false,
 		"can_export" => false,
-		"rewrite" => [ "slug" => "twsp_subscriber", "with_front" => false ],
+		"rewrite" => ["slug" => "twsp_subscriber", "with_front" => false],
 		"query_var" => true,
 		"supports" => false,
 		"show_in_graphql" => false,
 	];
 
-	register_post_type( "twsp_subscriber", $args );
+	register_post_type("twsp_subscriber", $args);
 }
 
-add_action( 'init', 'cptui_register_my_cpts_twsp_subscriber' );
+add_action('init', 'cptui_register_my_cpts_twsp_subscriber');
 
-function cptui_register_my_cpts_twsp_list() {
+function cptui_register_my_cpts_twsp_list()
+{
 
 	/**
 	 * Post Type: Lists.
 	 */
 
 	$labels = [
-		"name" => esc_html__( "Lists", "twentytwentyfive" ),
-		"singular_name" => esc_html__( "List", "twentytwentyfive" ),
+		"name" => esc_html__("Lists", "twentytwentyfive"),
+		"singular_name" => esc_html__("List", "twentytwentyfive"),
 	];
 
 	$args = [
-		"label" => esc_html__( "Lists", "twentytwentyfive" ),
+		"label" => esc_html__("Lists", "twentytwentyfive"),
 		"labels" => $labels,
 		"description" => "",
 		"public" => false,
@@ -64,7 +66,7 @@ function cptui_register_my_cpts_twsp_list() {
 		"rest_controller_class" => "WP_REST_Posts_Controller",
 		"rest_namespace" => "wp/v2",
 		"has_archive" => false,
-		"show_in_menu" => true,
+		"show_in_menu" => false,
 		"show_in_nav_menus" => true,
 		"delete_with_user" => false,
 		"exclude_from_search" => false,
@@ -72,13 +74,13 @@ function cptui_register_my_cpts_twsp_list() {
 		"map_meta_cap" => true,
 		"hierarchical" => false,
 		"can_export" => false,
-		"rewrite" => [ "slug" => "twsp_list", "with_front" => false ],
+		"rewrite" => ["slug" => "twsp_list", "with_front" => false],
 		"query_var" => true,
-		"supports" => [ "title" ],
+		"supports" => ["title"],
 		"show_in_graphql" => false,
 	];
 
-	register_post_type( "twsp_list", $args );
+	register_post_type("twsp_list", $args);
 }
 
-add_action( 'init', 'cptui_register_my_cpts_twsp_list' );
+add_action('init', 'cptui_register_my_cpts_twsp_list');
